f60b154bea8a238901e17cd094ea27ec4edfb40a
pyprism <git.pyprism@gmail.com>
2014-03-26 21:55:50 +0600

    Complete : backend task

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -13,9 +12,7 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -29,8 +26,13 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/random', routes.random);
 
+//funny 404 :D
+app.get('*', function(req, res){
+    res.sendfile('public/images/404.gif') ;;
+});
+//change the ip
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Express server listening on port ' +  app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,20 @@
 /*
  * GET home page.
  */
+var randomWords = require('random-words');
+
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.sendfile('public/index.html') ;
+};
+
+
+exports.random = function (req,res){
+    var hiren = randomWords({ exactly: 10, join: ' ' });
+    res.json({ random : hiren }) ;
+};
+
+
+exports.about = function(req ,res){
+  res.sendfile('public/about.html');
 };
