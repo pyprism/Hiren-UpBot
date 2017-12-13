bb769dbba19a4f271ced20d3f80683ee2c8715e3
Ashutosh Das <git.pyprism@gmail.com>
2017-12-13 21:08:57 +0600

    added new view

--- views/auth.go
+++ views/auth.go
@@ -29,3 +29,21 @@ func Login(c *gin.Context)  {
 
 	}
 }
+
+func SignUp(c *gin.Context) {
+	if c.Request.Method == "GET" {
+		c.HTML(http.StatusOK, "signup.tmpl", gin.H{})
+	} else if c.Request.Method == "POST" {
+		var form LoginForm
+		if err:=c.ShouldBind(&form); err == nil {
+			if form.User == "demo" && form.Password == "demo" {
+				c.HTML(http.StatusAccepted, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
+			} else {
+				c.HTML(http.StatusForbidden, "signup.tmpl", gin.H{"status": "Username/Password is not valid!"})
+			}
+		} else {
+			c.HTML(http.StatusBadRequest, "signup.tmpl", gin.H{"status": err.Error()})
+		}
+
+	}
+}
