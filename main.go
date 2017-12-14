92a13966f30a46ae4a596161ec87c223479269b8
Ashutosh Das <git.pyprism@gmail.com>
2017-12-14 11:38:00 +0600

    added pkg config

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pyprism/Hiren-UpBot/views"
+	"github.com/spf13/viper"
+	"log"
 	"os"
 )
-
-
 
 func main() {
 	router := gin.Default()
 	router.Static("/static", "./static")
 	router.LoadHTMLGlob("templates/*")
 
+	// config file
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	// database
+	db, err := gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
+		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// routers
 	router.GET("/", views.Login)
 	router.POST("/", views.Login)
 	router.GET("/signup/", views.SignUp)
 	router.POST("/signup/", views.SignUp)
 
-	PORT := os.Getenv("PORT")
-	log.Fatal(router.Run(PORT))
+	log.Fatal(router.Run(viper.GetString("port")))
 }
-
-
