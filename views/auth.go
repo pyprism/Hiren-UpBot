8304d8931ed1eac9507c6f4c060acb6f9bf42953
Ashutosh Das <git.pyprism@gmail.com>
2017-12-14 19:25:43 +0600

    wip

--- views/auth.go
+++ views/auth.go
@@ -1,14 +1,39 @@
 package views
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pyprism/Hiren-UpBot/models"
+	"github.com/spf13/viper"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	//"golang.org/x/crypto/bcrypt"
+	//"github.com/pyprism/Hiren-UpBot/models"
 )
 
 type LoginForm struct {
 	User string `form:"username" binding:"required"`
 	Password string `form:"password" binding:"required"`
+}
+
+var db *gorm.DB
+
+func init()  {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	db, err := gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
+		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
 }
 
 
@@ -32,6 +57,10 @@ func Login(c *gin.Context)  {
 
 func SignUp(c *gin.Context) {
 	if c.Request.Method == "GET" {
+		var user []models.User
+		db.Count(&user)
+		fmt.Println(user)
+
 		c.HTML(http.StatusOK, "signup.tmpl", gin.H{})
 	} else if c.Request.Method == "POST" {
 		var form LoginForm
