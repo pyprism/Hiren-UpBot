a0f0514bfb254764f950f5011a113e7ae36a1f82
Ashutosh Das <git.pyprism@gmail.com>
2017-12-19 16:23:25 +0600

    wip

--- views/auth.go
+++ views/auth.go
@@ -5,22 +5,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pyprism/Hiren-UpBot/models"
-	"github.com/spf13/viper"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pyprism/Hiren-UpBot/models"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/bcrypt"
 	//"golang.org/x/crypto/bcrypt"
 	//"github.com/pyprism/Hiren-UpBot/models"
 )
 
 type LoginForm struct {
-	User string `form:"username" binding:"required"`
+	User     string `form:"username" binding:"required"`
 	Password string `form:"password" binding:"required"`
 }
 
 var db *gorm.DB
 
-func init()  {
+func init() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
@@ -34,18 +35,24 @@ func init()  {
 		panic("failed to connect database")
 	}
 	defer db.Close()
-	var user []models.User
-	db.Count(&user)
-	fmt.Println(user)
 }
 
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
 
-func Login(c *gin.Context)  {
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func Login(c *gin.Context) {
 	if c.Request.Method == "GET" {
 		c.HTML(http.StatusOK, "login.tmpl", gin.H{})
 	} else if c.Request.Method == "POST" {
 		var form LoginForm
-		if err:=c.ShouldBind(&form); err == nil {
+		if err := c.ShouldBind(&form); err == nil {
 			if form.User == "demo" && form.Password == "demo" {
 				c.HTML(http.StatusAccepted, "login.tmpl", gin.H{"status": "connected"})
 			} else {
@@ -63,12 +70,27 @@ func SignUp(c *gin.Context) {
 		c.HTML(http.StatusOK, "signup.tmpl", gin.H{})
 	} else if c.Request.Method == "POST" {
 		var form LoginForm
-		if err:=c.ShouldBind(&form); err == nil {
-			if form.User == "demo" && form.Password == "demo" {
-				c.HTML(http.StatusAccepted, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
-			} else {
-				c.HTML(http.StatusForbidden, "signup.tmpl", gin.H{"status": "Username/Password is not valid!"})
-			}
+		if err := c.ShouldBind(&form); err == nil {
+			fmt.Println(form.Password)
+			hash, _ := HashPassword(form.Password)
+			fmt.Println(hash)
+
+			var count int64
+			db.Model(&models.User{}).Count(&count)
+			fmt.Println(count)
+			//
+			//if count == 0 {
+			//	hash, err := HashPassword(form.Password)
+			//	if err != nil {
+			//		panic(err)
+			//	}
+			//	user := models.User{Name: form.User, Password: hash, Admin: true}
+			//	db.Create(&user)
+			//	c.HTML(http.StatusAccepted, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
+			//}
+
+			c.HTML(http.StatusForbidden, "signup.tmpl", gin.H{"status": "Username/Password is not valid!"})
+
 		} else {
 			c.HTML(http.StatusBadRequest, "signup.tmpl", gin.H{"status": err.Error()})
 		}
