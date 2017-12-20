16cf7ca268a1e366410cb7746bf1e30360f7981e
Ashutosh Das <git.pyprism@gmail.com>
2017-12-20 13:17:24 +0600

    added db

---
+++ db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"github.com/go-xorm/xorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pyprism/Hiren-UpBot/models"
+	"github.com/spf13/viper"
+)
+
+var db *xorm.Engine
+var err error
+
+func Init() {
+	db, err = xorm.NewEngine("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
+		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//defer db.Close()
+	db.ShowSQL(true)
+	db.DropTables(&models.User{})
+	db.CreateTables(&models.User{})
+}
+
+func GetDB() *xorm.Engine {
+	return db
+}
+
+//func CloseDB() {
+//	db.Close()
+//}
