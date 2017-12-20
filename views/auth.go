494f0683dd99d37571b51bad32c6eac159611580
Ashutosh Das <git.pyprism@gmail.com>
2017-12-20 13:17:14 +0600

    wip

--- views/auth.go
+++ views/auth.go
@@ -5,13 +5,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/pyprism/Hiren-UpBot/db"
 	"github.com/pyprism/Hiren-UpBot/models"
-	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
-	//"golang.org/x/crypto/bcrypt"
-	//"github.com/pyprism/Hiren-UpBot/models"
 )
 
 type LoginForm struct {
@@ -19,23 +15,30 @@ type LoginForm struct {
 	Password string `form:"password" binding:"required"`
 }
 
-var db *gorm.DB
+//var db *gorm.DB
+//
+//func init() {
+//	viper.SetConfigName("config")
+//	viper.AddConfigPath(".")
+//	err := viper.ReadInConfig()
+//	if err != nil { // Handle errors reading the config file
+//		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+//	}
+//
+//	db, err := gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
+//		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
+//	if err != nil {
+//		panic("failed to connect database")
+//	}
+//	//defer db.Close()
+//}
 
-func init() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil { // Handle errors reading the config file
-		panic(fmt.Errorf("Fatal error config file: %s \n", err))
-	}
-
-	db, err := gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
-		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
-	if err != nil {
-		panic("failed to connect database")
-	}
-	defer db.Close()
-}
+//func x() {
+//	var dbx = db.GetDB()
+//	user := new(models.User)
+//	total, err := dbx.Count(user)
+//	fmt.Println(total, err)
+//}
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -71,25 +74,47 @@ func SignUp(c *gin.Context) {
 	} else if c.Request.Method == "POST" {
 		var form LoginForm
 		if err := c.ShouldBind(&form); err == nil {
-			fmt.Println(form.Password)
-			hash, _ := HashPassword(form.Password)
-			fmt.Println(hash)
 
-			var count int64
-			db.Model(&models.User{}).Count(&count)
-			fmt.Println(count)
-			//
-			//if count == 0 {
-			//	hash, err := HashPassword(form.Password)
-			//	if err != nil {
-			//		panic(err)
-			//	}
-			//	user := models.User{Name: form.User, Password: hash, Admin: true}
-			//	db.Create(&user)
-			//	c.HTML(http.StatusAccepted, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
-			//}
+			var hiren = db.GetDB()
+			user := new(models.User)
+			count, err := hiren.Count(user)
+			if err != nil {
+				panic(err)
+			}
 
-			c.HTML(http.StatusForbidden, "signup.tmpl", gin.H{"status": "Username/Password is not valid!"})
+			if count == 0 {
+				hash, err := HashPassword(form.Password)
+				if err != nil {
+					panic(err)
+				}
+				user := new(models.User)
+				user.UserName = form.User
+				user.Password = hash
+				user.Admin = true
+				affected, err := hiren.Insert(user)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(affected)
+				fmt.Println(user.Id)
+				c.HTML(http.StatusAccepted, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
+			} else {
+				hash, err := HashPassword(form.Password)
+				if err != nil {
+					panic(err)
+				}
+				user := new(models.User)
+				user.UserName = form.User
+				user.Password = hash
+				user.Admin = false
+				affected, err := hiren.Insert(user)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(affected)
+				fmt.Println(user.Id)
+				c.HTML(http.StatusForbidden, "signup.tmpl", gin.H{"status": "Signed up successfully!"})
+			}
 
 		} else {
 			c.HTML(http.StatusBadRequest, "signup.tmpl", gin.H{"status": err.Error()})
