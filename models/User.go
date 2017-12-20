6eb5be04ec11f58aedc11656b846fc713a393912
Ashutosh Das <git.pyprism@gmail.com>
2017-12-20 13:17:07 +0600

    rewrite

--- models/User.go
+++ models/User.go
@@ -1,14 +1,12 @@
 package models
 
-import (
-	"time"
-)
+import "time"
 
 type User struct {
-	ID        uint `gorm:"primary_key"`
-	Name      string
-	Password  string
-	Admin     bool
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id        int64
+	UserName  string    `xorm:"unique varchar(25) not null"`
+	Password  string    `xorm:"varchar(255) not null"`
+	Admin     bool      `xorm:"not null"`
+	CreatedAt time.Time `xorm:"created"`
+	UpdatedAt time.Time `xorm:"updated"`
 }
