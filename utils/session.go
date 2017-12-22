e5f7481013c31ee3188aa3722a0cd2ba332f87ac
Ashutosh Das <git.pyprism@gmail.com>
2017-12-22 16:13:15 +0600

    wip

--- utils/session.go
+++ utils/session.go
@@ -7,23 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-
 func SetSession(userName string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
 		session.Set("username", userName)
-
-		var count int
-		v := session.Get("count")
-		if v == nil {
-			count = 0
-		} else {
-			count = v.(int)
-			fmt.Println(count)
-			count++
-		}
-		session.Set("count", count)
+		//
+		//var count int
+		//v := session.Get("count")
+		//if v == nil {
+		//	count = 0
+		//} else {
+		//	count = v.(int)
+		//	fmt.Println(count)
+		//	count++
+		//}
+		//session.Set("count", count)
+		fmt.Println(session.Get("username"))
 		session.Save()
-		c.Next()
+		//c.Next()
 	}
 }
