ec8087823ab6ba28c87c450854b6ff1cc41cf578
Ashutosh Das <git.pyprism@gmail.com>
2017-12-22 20:45:49 +0600

    changed orm :/

--- db/db.go
+++ db/db.go
@@ -1,32 +1,29 @@
 package db
 
 import (
-	"github.com/go-xorm/xorm"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pyprism/Hiren-UpBot/models"
 	"github.com/spf13/viper"
 )
 
-var db *xorm.Engine
+var db *gorm.DB
 var err error
 
 func Init() {
-	db, err = xorm.NewEngine("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
+	db, err = gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
 		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
 	if err != nil {
 		panic("failed to connect database")
 	}
-	//defer db.Close()
-	db.ShowSQL(true)
+	db.LogMode(true)
 	//db.DropTables(&models.User{})
-	db.CreateTables(&models.User{})
-	db.CreateUniques(new(models.User))
+	//defer db.Close()
+
+	db.AutoMigrate(&models.User{})
 }
 
-func GetDB() *xorm.Engine {
+func GetDB() *gorm.DB {
 	return db
 }
 
-//func CloseDB() {
-//	db.Close()
-//}
