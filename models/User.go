5a426eb7dd0efe6582f5034e1e7480b097fc077c
Ashutosh Das <git.pyprism@gmail.com>
2017-12-22 20:46:04 +0600

    refactored user

--- models/User.go
+++ models/User.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	Id        int64
-	UserName  string    `xorm:"unique varchar(25) not null"`
-	Password  string    `xorm:"varchar(255) not null"`
-	Admin     bool      `xorm:"not null"`
-	CreatedAt time.Time `xorm:"created"`
-	UpdatedAt time.Time `xorm:"updated"`
+	ID        uint `gorm:"primary_key"`
+	UserName  string    `gorm:"not null;unique"`
+	Password  string    `gorm:"not null"`
+	Admin     bool      `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
