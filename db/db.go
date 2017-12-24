63fe7ce8112ac4a769f1016b1f3080da9f60814e
Ashutosh Das <git.pyprism@gmail.com>
2017-12-24 11:24:36 +0600

    refactored db

--- db/db.go
+++ db/db.go
@@ -1,18 +1,29 @@
 package db
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/pyprism/Hiren-UpBot/models"
 	"github.com/spf13/viper"
 )
 
+type Hiren struct {
+	Server string
+}
+
 var db *gorm.DB
 var err error
 
-func Init() {
-	db, err = gorm.Open("postgres", "host=localhost"+" user="+viper.GetString("db_user")+
-		" dbname="+viper.GetString("db_name")+" sslmode=disable"+" password="+viper.GetString("db_password"))
+func (h *Hiren) Connect() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	h.Server = "host=localhost" + " user=" + viper.GetString("db_user") + " dbname=" + viper.GetString("db_name") + " sslmode=disable" + " password=" + viper.GetString("db_password")
+	db, err = gorm.Open("postgres", h.Server)
 	if err != nil {
 		panic("failed to connect database")
 	}
@@ -27,3 +38,8 @@ func GetDB() *gorm.DB {
 	return db
 }
 
+func (h *Hiren) UserCount() int64 {
+	var count int64
+	db.Model(models.User{}).Count(&count)
+	return count
+}
