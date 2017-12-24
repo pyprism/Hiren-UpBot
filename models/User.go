0167b74cac4913c5512460a16581b08fe2dd6307
Ashutosh Das <git.pyprism@gmail.com>
2017-12-24 11:25:08 +0600

    .

--- models/User.go
+++ models/User.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type User struct {
-	ID        uint `gorm:"primary_key"`
-	UserName  string    `gorm:"not null;unique"`
-	Password  string    `gorm:"not null"`
-	Admin     bool      `gorm:"not null"`
+	ID        uint   `gorm:"primary_key"`
+	UserName  string `gorm:"not null;unique"`
+	Password  string `gorm:"not null"`
+	Admin     bool   `gorm:"not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
