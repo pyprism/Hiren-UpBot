cbd1947fc9118e81d734ef3ea12a0a929d461cb6
Ashutosh Das <git.pyprism@gmail.com>
2017-12-28 21:09:32 +0600

    refactored

--- utils/mailer.go
+++ utils/mailer.go
@@ -1,15 +1,22 @@
 package utils
 
-import  "gopkg.in/mailgun/mailgun-go.v1"
+import (
+	"fmt"
+	"log"
 
-mg := mailgun.NewMailgun(yourdomain, ApiKey, publicApiKey)
-message := mailgun.NewMessage(
-	"sender@example.com",
-	"Fancy subject!",
-	"Hello from Mailgun Go!",
-	"recipient@example.com")
-resp, id, err := mg.Send(message)
-if err != nil {
-	log.Fatal(err)
+	"gopkg.in/mailgun/mailgun-go.v1"
+)
+
+func Mail(domain, apiKey, publicAPIKey string) {
+	mg := mailgun.NewMailgun(domain, apiKey, publicAPIKey)
+	message := mailgun.NewMessage(
+		"sender@example.com",
+		"Fancy subject!",
+		"Hello from Mailgun Go!",
+		"recipient@example.com")
+	resp, id, err := mg.Send(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
 }
-fmt.Printf("ID: %s Resp: %s\n", id, resp)
