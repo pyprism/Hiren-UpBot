d94c76e16a3cd27fb2983c8b24d45bfbade20c66
Ashutosh Das <git.pyprism@gmail.com>
2018-01-05 22:56:29 +0600

    added new func

--- views/list.go
+++ views/list.go
@@ -1,6 +1,7 @@
 package views
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/flosch/pongo2"
@@ -17,5 +18,23 @@ func List(c echo.Context) error {
 		"username": sess.Values["username"],
 		"urls": urls,
 	}
-	return c.Render(http.StatusOK, "templates/list.html", data)
+	return c.Render(http.StatusFound, "templates/list.html", data)
+}
+
+func HostByID(c echo.Context) error {
+	id := c.Param("id")
+	log.Println("id xxxxxxxxxxxxxx: ", id)
+	sess, _ := session.Get("session", c)
+	var username , _ = sess.Values["username"].(string)
+	url, err := bunny.FindHostById(username, id)
+	if err !=nil {
+		return c.String(http.StatusNotFound, "nai :/")
+	} else {
+		data := pongo2.Context{
+			"title":    "Details of" + id,
+			"username": username,
+			"url": url,
+		}
+		return c.Render(http.StatusFound, "templates/host_by_id.html", data)
+	}
 }
