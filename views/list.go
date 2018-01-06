0f48e7140df24dfe98977f3495c8f5b4abb81ce8
Ashutosh Das <git.pyprism@gmail.com>
2018-01-06 11:34:39 +0600

    cleanup

--- views/list.go
+++ views/list.go
@@ -1,39 +1,36 @@
 package views
 
 import (
-	"log"
-	"net/http"
-
 	"github.com/flosch/pongo2"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
+	"net/http"
 )
 
 func List(c echo.Context) error {
 	sess, _ := session.Get("session", c)
-	var username , _ = sess.Values["username"].(string)
+	var username, _ = sess.Values["username"].(string)
 	urls := bunny.UrlList(username)
 	data := pongo2.Context{
 		"title":    "Add New Host",
 		"username": sess.Values["username"],
-		"urls": urls,
+		"urls":     urls,
 	}
 	return c.Render(http.StatusFound, "templates/list.html", data)
 }
 
 func HostByID(c echo.Context) error {
 	id := c.Param("id")
-	log.Println("id xxxxxxxxxxxxxx: ", id)
 	sess, _ := session.Get("session", c)
-	var username , _ = sess.Values["username"].(string)
+	var username, _ = sess.Values["username"].(string)
 	url, err := bunny.FindHostById(username, id)
-	if err !=nil {
+	if err != nil {
 		return c.String(http.StatusNotFound, "nai :/")
 	} else {
 		data := pongo2.Context{
-			"title":    "Details of" + id,
+			"title":    "Details of " + url.Name,
 			"username": username,
-			"url": url,
+			"url":      url,
 		}
 		return c.Render(http.StatusFound, "templates/host_by_id.html", data)
 	}
